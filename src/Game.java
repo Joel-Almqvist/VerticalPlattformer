56ec03124034e6a2c16ecb84511a278ba616de87
Joel Almqvist <joeal360@student.liu.se>
2018-06-24 09:43:34 +0200

    Fix gitignore to ignore subfolders

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,7 @@ public class Game{
         Board board = new Board(50,50);
         BoardVisualComponent comp = new BoardVisualComponent(board);
 	BoardVisualFrame frame = new BoardVisualFrame(board, "window name", comp);
+        System.out.println("Bye World!"); // Display the string.
         // TODO Fix prefered window size and such
     }
 
